a41ab5982b81cd30fa91081272c4301fa85c99dd
Jonibek <zayniddinovjonibek391@gmail.com>
2024-12-06 11:43:49 +0500

    Update main.py
    ver

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ def decimal_to_hexadecimal_using_math(decimal_number):
     import math
     hex_map = "0123456789ABCDEF"
     result = ""
-    while decimal_number > 0:
-        remainder = decimal_number % 16
-        result = hex_map[remainder] + result
-        decimal_number = math.floor(decimal_number / 16)
+    
     return result
 
 if __name__ == "__main__":
